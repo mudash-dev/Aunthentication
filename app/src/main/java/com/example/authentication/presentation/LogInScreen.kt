5f3570d3e73b1c6d59e31af7d34d93688e3a0551
mudash-dev <ashleymudambaji@gmail.com>
2023-07-26 15:36:02 +0300

    APP:Set up LogIn Screen and its UI Components.

---
+++ app/src/main/java/com/example/authentication/presentation/LogInScreen.kt
@@ -0,0 +1,140 @@
+package com.example.authentication.presentation
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Visibility
+import androidx.compose.material.icons.filled.VisibilityOff
+import androidx.compose.material.icons.rounded.Email
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.authentication.R
+
+@Composable
+fun LogInScreen() {
+    var password by remember {
+        mutableStateOf(value = "")
+    }
+    var showPassword by remember {
+        mutableStateOf(value = false)
+    }
+    LazyColumn(
+        contentPadding = PaddingValues(24.dp),
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White),
+    ) {
+        item {
+            OutlinedTextField(
+                value = "",
+                modifier = Modifier.fillMaxWidth(),
+                onValueChange = {},
+                label = { Text(text = stringResource(id = R.string.Email)) },
+                leadingIcon = { Icon(imageVector = Icons.Rounded.Email, contentDescription = null) },
+            )
+        }
+        item {
+            Spacer(modifier = Modifier.height(16.dp))
+        }
+        item {
+            OutlinedTextField(
+                value = password,
+                onValueChange = {
+                    password = it
+                },
+                modifier = Modifier.fillMaxWidth(),
+                label = { Text(text = stringResource(id = R.string.InputPassword)) },
+                visualTransformation = if (showPassword) {
+                    VisualTransformation.None
+                } else {
+                    PasswordVisualTransformation()
+                },
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+                trailingIcon = {
+                    if (showPassword) {
+                        IconButton(onClick = { showPassword = false }) {
+                            Icon(imageVector = Icons.Filled.Visibility, contentDescription = "hide_password")
+                        }
+                    } else {
+                        IconButton(
+                            onClick = { showPassword = true },
+                        ) {
+                            Icon(
+                                imageVector = Icons.Filled.VisibilityOff,
+                                contentDescription = "hide_password",
+                            )
+                        }
+                    }
+                },
+            )
+        }
+        item {
+            TextButton(onClick = { /*TODO*/ }) {
+                Text(
+                    text = stringResource(id = R.string.Forgot),
+                    textAlign = TextAlign.End,
+                    textDecoration = TextDecoration.Underline,
+                )
+            }
+        }
+        item {
+            Spacer(modifier = Modifier.height(16.dp))
+        }
+        item {
+            Button(
+                onClick = { /*TODO*/ },
+                modifier = Modifier.fillMaxWidth(),
+                shape = RoundedCornerShape(24.dp),
+            ) {
+                Text(text = stringResource(id = R.string.LogIn))
+            }
+        }
+        item {
+            Spacer(modifier = Modifier.height(32.dp))
+        }
+        item {
+            TextButton(onClick = { /*TODO*/ }) {
+                Text(
+                    text = stringResource(id = R.string.SignUp),
+                    textAlign = TextAlign.Center,
+                    textDecoration = TextDecoration.Underline,
+                )
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun LogInView() {
+    Column {
+        LogInScreen()
+    }
+}
